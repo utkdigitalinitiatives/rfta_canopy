750540e2ea369de88f24b441157c6df8bc81f134
Mat Jordan <mat@utk.edu>
2021-06-24 11:36:01 -0400

    Init RFTA.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rising from the Ashes`,
+    description: ``,
+    author: `University of Tennessee Libraries`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
