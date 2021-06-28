d5837322b28c6551def8822933233bd133b0b5d7
Mat Jordan <mat@utk.edu>
2021-06-28 13:02:16 -0400

    Refine viewer styling.

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -1,38 +1,41 @@
 import * as React from "react"
 import Mirador from "./Mirador"
 import PropTypes from "prop-types"
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 
 const Viewer = ({ id, manifestId }) => (
-  <div className="canopy-viewer">
-    <div className="canopy-mirador">
-      <Mirador
-        config={{
-          id: `canopy-mirador-${id}`,
-          window: {
-            hideWindowTitle: true,
-            sideBarOpen: false,
-            defaultSidebarPanelWidth: 320,
-            allowTopMenuButton: true,
-            allowWindowSideBar: true,
-            allowMaximize: false,
-            allowClose: false,
-            forceDrawAnnotations: true,
-            allowFullscreen: true,
-          },
-          windows: [{
-            manifestId: manifestId,
-          }],
-          workspaceControlPanel: {
-            enabled: false,
-          },
-        }}
-        plugins={[]}
-      />
+  <AspectRatio.Root ratio={100 / 38.2}>
+    <div className="canopy-viewer">
+      <div className="canopy-mirador">
+        <Mirador
+          config={{
+            id: `canopy-mirador-${id}`,
+            window: {
+              hideWindowTitle: true,
+              sideBarOpen: false,
+              defaultSidebarPanelWidth: 320,
+              allowTopMenuButton: true,
+              allowWindowSideBar: true,
+              allowMaximize: false,
+              allowClose: false,
+              forceDrawAnnotations: true,
+              allowFullscreen: true,
+            },
+            windows: [{
+              manifestId: manifestId,
+            }],
+            workspaceControlPanel: {
+              enabled: false,
+            },
+          }}
+          plugins={[]}
+        />
+      </div>
+      <aside className="canopy-navigator">
+        [navigator]
+      </aside>
     </div>
-    <aside className="canopy-navigator">
-      [navigator]
-    </aside>
-  </div>
+  </AspectRatio.Root>
 )
 
 Viewer.propTypes = {
