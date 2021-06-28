779b6fda99049476e0bef4f2f896c38421a8f2d3
Mat Jordan <mat@utk.edu>
2021-06-28 11:26:03 -0400

    Cleanup config, remove unnused plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,22 +17,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-lunr`,
       options: {
@@ -53,7 +38,7 @@ module.exports = {
         },
         filename: 'searchIndex.json',
         fetchOptions: {
-          credentials: 'same-origin'
+          credentials: 'same-origin',
         },
       },
     },
