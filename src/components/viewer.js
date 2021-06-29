b7145bb5bb7706b784181f1a1327b9699ce7f3b2
Mat Jordan <mat@utk.edu>
2021-06-29 15:50:49 -0400

    Update aspect ratio.

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -9,7 +9,7 @@ const Viewer = ({ id, manifestId }) => (
   <>
     <div className="canopy-viewer">
       <div className="canopy-mirador">
-        <AspectRatio.Root ratio={16 / 9}>
+        <AspectRatio.Root ratio={4 / 3}>
           <Mirador
             config={{
               id: `canopy-mirador-${id}`,
