454ac26782a045a0223917dfa6561d1fd941b2b9
Mark Baggett <mbagget1@utk.edu>
2021-08-17 18:25:33 -0400

    Refactor Description around MetadataElement.

--- src/components/canopy/Description.js
+++ src/components/canopy/Description.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MetadataElement from "./MetadataElement"
 
 class Description extends Component {
   constructor(props) {
@@ -8,12 +9,12 @@ class Description extends Component {
 
   handleMetadata () {
     const metadata = this.props.node.metadata
-    let description = this
     return (metadata.map(function(element){
       return (
         <>
-          <dt>{description.getLabel(element.label)}</dt>
-          {description.getValues(element.value.en)}
+          <MetadataElement element={element}
+                           language='en'
+          />
         </>
       )
     }
@@ -23,20 +24,11 @@ class Description extends Component {
   handleRequiredStatement () {
     return (
       <>
-        <dt>{this.props.node.requiredStatement.label.en[0]}</dt>
-        <dd>{this.props.node.requiredStatement.value.en[0]}</dd>
+          <MetadataElement element={this.props.node.requiredStatement}
+                           language='en'
+          />
       </>
     )
-  }
-
-  getLabel (label) {
-    return label.en[0]
-  }
-
-  getValues(multiple_values) {
-    return multiple_values.map(function(value) {
-      return <dd>{value}</dd>
-    })
   }
 
   render() {
