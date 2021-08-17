98f40c1139ffae6e12fc146d76b52efa02c9f7b1
Mark Baggett <mbagget1@utk.edu>
2021-08-17 18:20:06 -0400

    Initialize MetadataElement.

---
+++ src/components/canopy/MetadataElement.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class MetadataElement extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  parseElement () {
+    return(
+      <>
+        {this.getLabel(this.props.element.label)}
+        {this.getValues(this.props.element.value)}
+      </>
+    )
+  }
+
+  getLabel (label) {
+    return <dt>{label[this.props.language][0]}</dt>
+  }
+
+  getValues(multiple_values) {
+    return multiple_values[this.props.language].map(function(value) {
+      return <dd>{value}</dd>
+    })
+  }
+
+  render() {
+
+    return (
+      this.parseElement()
+    )
+  }
+}
+
+export default MetadataElement;
