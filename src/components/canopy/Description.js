8dfefe15d4333d6c4137f7db190b433da4bc33b0
Mark Baggett <mbagget1@utk.edu>
2021-08-17 17:45:22 -0400

    Correct markup with React fragments.

--- src/components/canopy/Description.js
+++ src/components/canopy/Description.js
@@ -5,19 +5,16 @@ class Description extends Component {
     super(props);
   }
 
-  componentDidMount () {
-
-  }
 
   handleMetadata () {
     const metadata = this.props.node.metadata
     let description = this
     return (metadata.map(function(element){
       return (
-        <div>
+        <>
           <dt>{description.getLabel(element.label)}</dt>
           {description.getValues(element.value.en)}
-        </div>
+        </>
       )
     }
     ));
@@ -25,10 +22,10 @@ class Description extends Component {
 
   handleRequiredStatement () {
     return (
-      <div>
+      <>
         <dt>{this.props.node.requiredStatement.label.en[0]}</dt>
         <dd>{this.props.node.requiredStatement.value.en[0]}</dd>
-      </div>
+      </>
     )
   }
 
@@ -37,18 +34,15 @@ class Description extends Component {
   }
 
   getValues(multiple_values) {
-    console.log(multiple_values)
     return multiple_values.map(function(value) {
-      return (
-        <dd>{value}</dd>
-      )
+      return <dd>{value}</dd>
     })
   }
 
   render() {
 
     return (
-      <dl>
+      <dl className="canopy-metadata">
         {this.handleMetadata()}
         {this.handleRequiredStatement()}
       </dl>
