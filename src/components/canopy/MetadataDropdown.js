7e98d7cfad80add7dc4ee1d88c41093a6d014416
Mat Jordan <mat@utk.edu>
2021-08-19 16:53:17 -0400

    Add query fr dropdown and output of content values.

--- src/components/canopy/MetadataDropdown.js
+++ src/components/canopy/MetadataDropdown.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
 import { BiChevronDown } from "react-icons/bi"
+import { StaticQuery, graphql } from "gatsby"
+import { countBy } from 'lodash';
 
 class MetadataDropdown extends Component {
   constructor(props) {
@@ -19,12 +21,60 @@ class MetadataDropdown extends Component {
             <BiChevronDown />
           </DropdownMenu.Trigger>
           <DropdownMenu.Content>
-            xyz
+            <StaticQuery
+              query={graphql`
+                query {
+                  allManifests {
+                    edges {
+                      node {
+                        metadata {
+                          label {
+                            en
+                          }
+                          value {
+                            en
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              `}
+              render = {
+                data => (
+                  <MetadataDropdownContent data={data}
+                                           label={label} />
+                )
+              }
+            />
           </DropdownMenu.Content>
         </DropdownMenu.Root>
       </div>
     )
   }
+}
+
+const MetadataDropdownContent = ({ data, label }) => {
+  let values = []
+  const counts = {}
+  for (const result of data.allManifests.edges) {
+    for (const elemMatch of result.node.metadata) {
+      if (elemMatch.label.en[0] === label) {
+        for (const string of elemMatch.value.en) {
+          values.push(string)
+        }
+      }
+    }
+  }
+  values.sort();
+  const content = countBy(values)
+  return Object.keys(content).map((value) => {
+    return <MetadataDropdownContentItem value={value} count={content[value]}/>
+  });
+}
+
+const MetadataDropdownContentItem = ({ value, count }) => {
+  return <a><span>{count}</span> {value}</a>
 }
 
 export default MetadataDropdown;
