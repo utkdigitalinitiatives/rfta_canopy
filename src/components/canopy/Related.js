34069e816361280703d6d43b41851da25a213ab1
Mat Jordan <mat@utk.edu>
2021-08-23 11:36:33 -0400

    Minimally style related placeholders.

--- src/components/canopy/Related.js
+++ src/components/canopy/Related.js
@@ -7,11 +7,42 @@ class Related extends Component {
     super(props);
   }
 
+  relatedItem = (item) => {
+    return (
+      <figure>
+        <div>
+
+        </div>
+        <figcaption>{item}</figcaption>
+      </figure>
+    )
+  }
+
+  relatedItems = (items) => {
+    return items.map ((item) => {
+      return <li className="canopy-related-item">{this.relatedItem(item)}</li>
+    })
+  }
+
+  relatedRow = (label) => {
+    return (
+      <div className="canopy-related-row">
+        <span>{label}</span>
+        <ul>
+          {this.relatedItems(["Item A", "Item B", "Item C", "Item D"])}
+        </ul>
+      </div>
+    )
+  }
+
   render() {
 
     return (
       <div className="canopy-related">
-        [related items?]
+        <h2>Related Items</h2>
+        {this.relatedRow('Evacuation of civilians')}
+        {this.relatedRow('Pi Beta Phi Elementary School')}
+        {this.relatedRow('More Video Interviews')}
       </div>
     )
   }
