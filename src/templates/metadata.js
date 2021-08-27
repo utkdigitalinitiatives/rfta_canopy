c3956afa6ee3d114398fc3e29d4628456fc24a6c
Mat Jordan <mat@utk.edu>
2021-08-26 20:44:10 -0400

    Build baseline Metadata template.

--- src/templates/metadata.js
+++ src/templates/metadata.js
@@ -1,8 +1,11 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import Seo from "../components/layout/seo"
+import { graphql } from "gatsby"
 
 const Metadata = ({ data }) => {
+
+  console.log(data)
 
   return (
     <Layout>
@@ -12,3 +15,33 @@ const Metadata = ({ data }) => {
 }
 
 export default Metadata
+
+export const metadataQuery = graphql`
+  query allMetadata($id: String) {
+    allMetadata(
+      filter: {
+        slug: {
+          eq: $id
+        }
+      }
+    ) {
+      edges {
+        node {
+          label
+          slug
+          values {
+            label
+            slug
+            manifests {
+              id
+              label {
+                en
+              }
+              manifestId
+            }
+          }
+        }
+      }
+    }
+  }
+`
