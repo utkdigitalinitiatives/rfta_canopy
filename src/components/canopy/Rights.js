de6c4e5e5dda17ba5d09d95baa45fe61f6eada84
Mark Baggett <mbagget1@utk.edu>
2021-09-17 17:28:34 -0400

    Class figures.

--- src/components/canopy/Rights.js
+++ src/components/canopy/Rights.js
@@ -18,7 +18,7 @@ class Rights extends Component {
     let rights_identifier = this.props.rights.split('/')[4]
     if (this.props.rights.includes('creativecommons')) {
       return (
-        <figure>
+        <figure class="rights-statement">
           <a href={this.props.rights}>
             <img src={`https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/${rights_identifier}.svg`} alt={rights_identifier}/>
           </a>
@@ -27,7 +27,7 @@ class Rights extends Component {
     }
     else {
       return (
-        <figure>
+        <figure class="rights-statement">
           <a href={this.props.rights}>
             <img src={`https://rightsstatements.org/files/buttons/${rights_identifier}.dark-white-interior-blue-type.svg`} alt={rights_identifier}/>
           </a>
