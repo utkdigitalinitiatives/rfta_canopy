af97a8cfece332d59ff52f7dd71062e49757d556
Alisha Evans <alisha@notch8.com>
2021-11-15 18:03:11 -0600

    comment and formatting

--- src/components/canopy/Navbar.js
+++ src/components/canopy/Navbar.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 import Logo from "../../images/utk-logo.png"
 
 const Navbar = () => (
@@ -7,16 +7,38 @@ const Navbar = () => (
       <a className="navbar-brand" href="/">
         <img className= "logo-image" src={ Logo } />
       </a>
+      {/* this button is to render the hamburger menu on mobile devices */}
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/" onClick={() => console.log('hello there!')} >Browse</a>
+            <a
+              className="nav-link"
+              aria-current="page"
+              href="/"
+            >
+              Browse
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="/about">About</a>
+            <a
+              className="nav-link"
+              aria-current="page"
+              href="/about"
+            >
+              About
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/search">Search</a>
+            <a
+              className="nav-link"
+              aria-current="page"
+              href="/search"
+            >
+              Search
+            </a>
           </li>
         </ul>
       </div>
