c497795274c59922bd74d2d2c25624260910f72e
Alisha Evans <alisha@notch8.com>
2021-11-16 15:15:11 -0600

    add strings around search query

--- src/components/layout/search-limits.js
+++ src/components/layout/search-limits.js
@@ -5,7 +5,7 @@ const SearchLimits = ({ q }) => {
 return(
 <div className="search-limits-section px-5">
   <h4>
-    {q.length ? `Your Search Results for ${q}` : "Explore all resources"}
+    {q.length ? `Your Search Results for "${q}"` : "Explore all resources"}
   </h4>
   {/* We will need to add a conditional here as well depending if the search is filtered: */}
   <h6>Filtering by:</h6>
