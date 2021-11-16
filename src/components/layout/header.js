8b2022147615818777f66e6193459a859566969a
Alisha Evans <alisha@notch8.com>
2021-11-16 11:26:15 -0600

    change header height based on pathname

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -7,9 +7,10 @@ import SearchForm from "./search-form"
 
 const Header = ({ location, siteDescription, siteTitle }) => {
   const { q } = searchParams(location.search)
+  const homepage = location.pathname.length === 1
 
   return (
-    <header className="canopy-header d-flex flex-column p-5">
+    <header className={`canopy-header ${homepage ? 'tall': 'short'} d-flex flex-column p-5`}>
       <Link to={'/'}>
         <h1 className="canopy-title text-center text-md-start">{siteTitle}</h1>
       </Link>
@@ -17,10 +18,12 @@ const Header = ({ location, siteDescription, siteTitle }) => {
       <div className="canopy-form">
         <SearchForm initialQuery={q} />
       </div>
-      <div className="canopy-caption ms-auto mt-auto text-end text-white text-center text-md-start">
-        Rising from the Ashes documents the immediate and ongoing impacts of the 2016 Chimney Tops II wildfires, one of the largest natural disasters in Tennessee history.
-        <a className="text-warning"> Learn More.</a>
-      </div>
+      {homepage && (
+        <div className="canopy-caption ms-auto mt-auto text-end text-white text-center text-md-start">
+          Rising from the Ashes documents the immediate and ongoing impacts of the 2016 Chimney Tops II wildfires, one of the largest natural disasters in Tennessee history.
+          <a className="text-warning"> Learn More.</a>
+        </div>
+      )}
     </header>
   )
 }
