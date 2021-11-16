6f20dbf32dba797aef8f927b6f4cf426cd2b81db
Alisha Evans <alisha@notch8.com>
2021-11-16 10:06:17 -0600

    move search params into utility file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,21 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import Sticky from "react-sticky-el"
 
 import Layout from "../components/layout/layout"
 import Seo from "../components/layout/seo"
-import SearchForm from "../components/layout/search-form"
 import SearchResults from "../components/layout/search-results"
 import SearchLimits from "../components/layout/search-limits"
 import Facets from "../components/layout/facets"
+import { searchParams } from '../utilities/search'
 
 
 const IndexPage = ({ data, location }) => {
-
-  const params = new URLSearchParams(location.search.slice(1))
-  const q = params.get("q") || ""
-  const filter = params.get("filter") || ""
+  const { q, filter } = searchParams(location.search)
 
   return (
     <Layout location={location}>
       <Seo title="Search results" />
-      <div className="canopy-form">
-        <Sticky className="canopy-sticky">
-          <SearchForm initialQuery={q} />
-        </Sticky>
-      </div>
-      < SearchLimits />
+      <SearchLimits />
       <div className="canopy-main canopy-main-inner">
         <Facets />
         <SearchResults data={data} initialQuery={q} filter={filter} />
