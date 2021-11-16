561401a67099a777221bbd8cb9a2ff5aeb548eae
summer-cook <summercook@bellsouth.net>
2021-11-15 18:07:58 -0800

    setup netlify/gatsby form

--- src/components/layout/Contact.js
+++ src/components/layout/Contact.js
@@ -10,10 +10,15 @@ const Contact = () => {
             <p>If you would like to share your story, contact us via this form. We would love to add your story to our collection of resources.</p>
           </div>
           <div className="contact-form col-11 col-md-5 pl-md-1 text-center">
-            <input className="form-control form-control my-1" type="text" placeholder="Name" aria-label="Name"/>
-            <input className="form-control form-control my-1" type="text" placeholder="Email" aria-label="Email"/>
-            <textarea className="form-control form-control my-1" type="text" placeholder="Message" aria-label="Name"/>
-            <button type="submit" className="btn btn-secondary mt-1">Send</button>
+            <form name="Rising From The Ashes Contact Form" method="post" netlify-honeypot="bot-field" data-netlify="true" action="#">
+              <input type="hidden" name="form-name" value="Rising From The Ashes Contact Form" />
+              <input type="hidden" name="bot-field" />
+              <input className="form-control form-control my-1" type="text" placeholder="Name" aria-label="Name" name="name"/>
+              <input className="form-control form-control my-1" type="text" placeholder="Email" aria-label="Email" name="email"/>
+              <textarea className="form-control form-control my-1" type="text" placeholder="Message" aria-label="Message" name="message" />
+              <button type="submit" className="btn btn-secondary mt-1">Send</button>
+              <input type="reset" value="Clear" />
+            </form>
           </div>
         </div>
         <div className="align-items-start py-5 mx-3 mx-md-5 text-center">
