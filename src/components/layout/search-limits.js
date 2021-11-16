5c75780b7fee6c822f0865abf923e9c019ec194d
Alisha Evans <alisha@notch8.com>
2021-11-16 20:22:33 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/components/layout/search-limits.js
+++ src/components/layout/search-limits.js
@@ -4,12 +4,9 @@ const SearchLimits = ({ q }) => {
 
 return(
 <div className="search-limits-section px-5">
-  {q.length > 0 &&
-    <h4>Your Search Results for "{q}"</h4>
-  }
-  {q.length == 0 &&
-    <h4>Explore all resources</h4>
-  }
+  <h4>
+    {q.length ? `Your Search Results for ${q}` : "Explore all resources"}
+  </h4>
   {/* We will need to add a conditional here as well depending if the search is filtered: */}
   <h6>Filtering by:</h6>
 
