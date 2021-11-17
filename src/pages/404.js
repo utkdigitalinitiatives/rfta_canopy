20498de40da6f2a4cbe304bdbbd552d3607a3e44
Alisha Evans <alisha@notch8.com>
2021-11-17 14:54:45 +0000

    formatting

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,21 +4,19 @@ import Seo from "../components/layout/seo"
 import { Link } from "gatsby"
 
 
-const NotFoundPage = ({ location }) => {
+const NotFoundPage = ({ location }) => (
+  <Layout location={location}>
+    <Seo title="404: Not found" />
+    <div className="m-5 text-center text-md-start h-100">
+      <h2>404: Not Found</h2>
+      <p>You just hit a route that doesn't exist... the sadness.</p>
+      <Link href="/">
+        <button className="btn btn-primary">
+          Back to Browse
+        </button>
+      </Link>
+    </div>
+  </Layout>
+)
 
-  return (
-    <Layout location={location}>
-      <Seo title="404: Not found" />
-      <div className="m-5 text-center text-md-start h-100">
-        <h2>404: Not Found</h2>
-        <p>You just hit a route that doesn't exist... the sadness.</p>
-        <Link href="/">
-          <button className="btn btn-primary">
-            Back to Browse
-          </button>
-        </Link>
-      </div>
-    </Layout>
-  )
-}
 export default NotFoundPage
