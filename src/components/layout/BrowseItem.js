b56de094019ea4b7e99267398eaf9f3926e1101f
Alisha Evans <alisha@notch8.com>
2021-11-23 09:58:54 -0600

    linting

--- src/components/layout/BrowseItem.js
+++ src/components/layout/BrowseItem.js
@@ -1,18 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const BrowseItem = ({ altText, image, path, subtitle}) => {
-  return (
-      <div className="col-11 col-sm-6 col-md-3 px-0">
-        <Link to={`/search?filter=${path}`} >
-          <div className= "browse-item">
-            <div className="mx-auto mb-4 browse-image">
-              <img src={image} class="card-img-top img-fluid" alt={altText}/ >
-            </div>
-            <p className="text-center px-2">{subtitle}</p>
-          </div>
-        </Link>
+const BrowseItem = ({ altText, image, path, subtitle }) => (
+  <div className="col-11 col-sm-6 col-md-3 px-0">
+    <Link to={`/search?filter=${path}`}>
+      <div className= "browse-item">
+        <div className="mx-auto mb-4 browse-image">
+          <img src={image} class="card-img-top img-fluid" alt={altText} />
+        </div>
+        <p className="text-center px-2">{subtitle}</p>
       </div>
-    )
-  }
+    </Link>
+  </div>
+)
+
 export default BrowseItem
