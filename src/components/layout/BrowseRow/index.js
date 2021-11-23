feb4c16731a89edc98d782b940892e4642311e57
Alisha Evans <alisha@notch8.com>
2021-11-23 16:42:13 -0600

    update import

--- src/components/layout/BrowseRow/index.js
+++ src/components/layout/BrowseRow/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import data from './data'
 import BrowseItem from '../BrowseItem'
-import placeholderImage from "../../../images/chimneytops2placeholder.png"
+import placeholderImage from "../../../images/chimney-tops-2-placeholder.png"
 
 const BrowseRow = ({ dataType, backgroundClass='' }) => {
   const results = data[dataType]
