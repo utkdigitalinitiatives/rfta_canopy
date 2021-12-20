922ea57d6e496ac48cdc076dc57eac3c60fa3de0
Mark Baggett <mbagget1@utk.edu>
2021-12-19 21:21:49 -0500

    Add missing descriptions for oral history to metatags.

--- src/templates/manifest.js
+++ src/templates/manifest.js
@@ -28,7 +28,7 @@ const Manifest = ({ data, location }) => {
 
   return (
     <Layout location={location}>
-      <Seo title={node.label.en[0]} schemaMarkup={schema}/>
+      <Seo title={node.label.en[0]} schemaMarkup={schema} description={node.summary.en[0]}/>
       <DigitalObject node={node} />
     </Layout>
   )
