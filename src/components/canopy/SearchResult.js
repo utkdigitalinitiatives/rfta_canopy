44c0f63a794cb972021d61dac392b3ba32e648fc
Mark Baggett <mbagget1@utk.edu>
2021-12-21 08:27:15 -0500

    Switch to an anchor.

--- src/components/canopy/SearchResult.js
+++ src/components/canopy/SearchResult.js
@@ -1,23 +1,21 @@
 import React from 'react'
-import { Link as GatsbyLink } from "gatsby"
 
 const SearchResult = ({ result }) => {
+  console.log(result)
   const { label, slug, summary, thumbnail } = result
   const slugWithSlash = slug.endsWith('/') ? slug : `${slug}/`;
   return (
     <article key={slug}>
-      <GatsbyLink
-        to={slugWithSlash}
-      >
+      <a href={slugWithSlash} >
         <figure className='results-thumbnails'>
           <img src={thumbnail} alt={label} />
         </figure>
-      </GatsbyLink>
+      </a>
       <div>
-        <GatsbyLink
-          to={slugWithSlash}>
+        <a
+          href={slugWithSlash}>
           <header>{label || slug}</header>
-        </GatsbyLink>
+        </a>
         <p>{summary}</p>
       </div>
     </article>
