84f7cf75e251793627b0fab15d4efff6ee325bbf
Mark Baggett <mbagget1@utk.edu>
2023-05-26 12:55:36 -0400

    Use gatsby gtag.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,26 @@ module.exports = {
     siteUrl: `https://rfta.lib.utk.edu/`
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-DN6TP2L65T",
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -29,5 +49,6 @@ module.exports = {
         policy: [{ userAgent: '*', allow: '/' }]
       }
     },
+
   ],
 }
